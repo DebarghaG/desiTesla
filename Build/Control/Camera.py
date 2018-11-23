0ba603b4555db239b7f547ecfff1bb208d3680c1
Shaky <shekhar@hostdude.org>
2018-11-24 01:13:31 +0530

    fixing the code

--- Build/Control/Camera.py
+++ Build/Control/Camera.py
@@ -1,13 +1,13 @@
 import picamera
 camera = picamera.PiCamera()
 
-def CaptureForTraining:
+def CaptureForTraining():
     i=0
     #250 millisecond delay between images
     #120,000 ms total runtime ( 2 minutes for training)
-    while(i<120000)
+    while(i<120000):
         timeinms=int((dt.datetime.utcnow() - dt.datetime(1970,1,1)).total_seconds() * 1000)
-        camera.capture(‘%s.jpg’, resize=(640, 480), timeinms)
+        camera.capture(timeinms+‘%s.jpg’, resize=(640, 480))
         i+=250
         #Sleeping for 250ms
         sleep(250)
