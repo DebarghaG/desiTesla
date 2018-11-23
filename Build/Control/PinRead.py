b6aa7fb52e64bdb982142064f000bc92029b9211
Debargha Ganguly <royal.debargha@gmail.com>
2018-11-24 02:29:59 +0530

    Removing minor bugs

--- Build/Control/PinRead.py
+++ Build/Control/PinRead.py
@@ -1,16 +1,16 @@
 import RPi.GPIO as GPIO
 
-def Direction()
-    left = GPIO.input(21):
-    right = GPIO.input(22):
-    forward = GPIO.input(23):
+def Direction():
+    left = GPIO.input(21)
+    right = GPIO.input(22)
+    forward = GPIO.input(23)
 
-    if(left== True and right== False and foward==True)
+    if(left== True and right== False and foward==True):
         direction = 'FL'
-    if(left== False and right== True and foward==True)
+    if(left== False and right== True and foward==True):
         direction = 'FR'
-    if(left== False and right== False and foward==True)
+    if(left== False and right== False and foward==True):
         direction = 'FF'
-    if(left== False and right== False and foward==False)
+    if(left== False and right== False and foward==False):
         direction = 'NN'
     return direction
