ff05ec7e98a711b738a45f1728dec37b1402beb4
Shekhar Chatterjee <shekhar.chatterjee_ug20@ashoka.edu.in>
2018-11-24 01:40:57 +0530

    Create the file

---
+++ Build/Control/ImageClassifier.py
@@ -0,0 +1,8 @@
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
