f4eb5de211a76c1f39714977f7506788b517d157
Debargha Ganguly <royal.debargha@gmail.com>
2018-11-24 10:27:47 +0530

    Making the data gathering phase just store directions for each image and not the timestamp

--- Build/Control/Camera.py
+++ Build/Control/Camera.py
@@ -7,13 +7,15 @@ def CaptureForTraining():
     i=0
     #250 millisecond delay between images
     #120,000 ms total runtime ( 2 minutes for training)
+    #480 images, with names stamped with both the timestamp and the direction of the car.
     while(i<120000):
-        timeinms=int((dt.datetime.utcnow() - dt.datetime(1970,1,1)).total_seconds() * 1000)
+        #timeinms=int((dt.datetime.utcnow() - dt.datetime(1970,1,1)).total_seconds() * 1000)
         dir = Direction()
-        camera.capture(str(timeinms) + dir + ".jpg", resize=(640, 480))
+        #camera.capture(str(timeinms) + dir + ".jpg", resize=(640, 480))
+        camera.capture(dir + ".jpg", resize=(640, 480))
         i+=250
         #Sleeping for 250ms
         sleep(250)
-        
+
 
     camera.resolution = (800, 600)
