750be512c639ca87b8215df3657adda2ddff45fc
Prajwal Seth <seth.prajwal@gmail.com>
2018-11-24 21:42:04 +0530

    Update Test-Camera.py

--- Build/Tests/Test-Camera.py
+++ Build/Tests/Test-Camera.py
@@ -3,7 +3,10 @@
 #Checking if the camera can capture images
 
 import picamera
+
 camera = picamera.PiCamera()
+camera.start_preview()
 camera.capture(‘snapshot.jpg’, resize=(640, 480))
+camera.stop_preview()
 
 camera.resolution = (800, 600)
