a437ba1c3e323e445450acc1954b91298c38a2b5
Prajwal Seth <seth.prajwal@gmail.com>
2018-11-27 15:55:35 +0530

    Fixed the DatacCollection File to be able to run without the hardware short and Python PinRead file

--- Build/DataGathering/DataCollection.py
+++ Build/DataGathering/DataCollection.py
@@ -4,8 +4,9 @@ This piece of code is meant to be run during the data collection phase .
 It captures images with the timestamp and the direction to label the files.
 """
 
+from time import sleep
 import picamera
-from PinRead import Direction
+#from PinRead import Direction
 import datetime as dt
 camera = picamera.PiCamera()
 
@@ -16,13 +17,12 @@ def CaptureForTraining():
     #480 images, with names stamped with both the timestamp and the direction of the car.
     while(i<120000):
         timeinms=int((dt.datetime.utcnow() - dt.datetime(1970,1,1)).total_seconds() * 1000)
-        dir = Direction()
-        camera.capture(dir+ str(timeinms) + ".jpg", resize=(640, 480))
+        #dir = Direction()
+        camera.capture("""dir+ """ "%s"  %timeinms + ".jpg", resize=(640, 480))
         #camera.capture(dir + ".jpg", resize=(640, 480))
         i+=250
         #Sleeping for 250ms
-        sleep(250)
-
+        sleep(0.25)
 
     camera.resolution = (800, 600)
 
