6c894bedfbb20f8256778d6c868f10453f4e4d1e
Debargha Ganguly <royal.debargha@gmail.com>
2018-11-29 17:02:03 +0530

    Changing the motor driver library, hoping it to work now

--- Build/Tests/Test-Controls.py
+++ Build/Tests/Test-Controls.py
@@ -1,49 +1,69 @@
 import sys
 import time
 import RPi.GPIO as GPIO
+from gpiozero import PWMOutputDevice
 
-mode=GPIO.getmode()
+#mode=GPIO.getmode()
 
 Forward=13
 Backward=12
 Right = 24
 Left = 23
 
+forward_motor = OutputDevice(Forward)
+backward_motor = OutputDevice(Backward)
+right_motor = OutputDevice(Right)
+left_motor = OutputDevice(Left)
+
 sleeptime=1
 
+"""
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(Forward, GPIO.OUT)
 GPIO.setup(Backward, GPIO.OUT)
 GPIO.setup(Right, GPIO.OUT)
 GPIO.setup(Left, GPIO.OUT)
+"""
 
 def forward(x):
-    GPIO.output(Forward, GPIO.HIGH)
+    #GPIO.output(Forward, GPIO.HIGH)
+    forward_motor.on()
     print("Moving Forward")
     time.sleep(x)
-    GPIO.output(Forward, GPIO.LOW)
+    forward_motor.off()
+    #GPIO.output(Forward, GPIO.LOW)
 
 def reverse(x):
-    GPIO.output(Backward, GPIO.HIGH)
+    #GPIO.output(Backward, GPIO.HIGH)
+    backward_motor.on()
     print("Moving Backward")
     time.sleep(x)
-    GPIO.output(Backward, GPIO.LOW)
+    backward_motor.off()
+    #GPIO.output(Backward, GPIO.LOW)
 
 def right(x):
-    GPIO.output(Forward, GPIO.HIGH)
-    GPIO.output(Right, GPIO.HIGH)
+    #GPIO.output(Forward, GPIO.HIGH)
+    #GPIO.output(Right, GPIO.HIGH)
+    forward_motor.on()
+    right_motor.on()
     print("Moving Right")
     time.sleep(x)
-    GPIO.output(Forward, GPIO.LOW)
-    GPIO.output(Right, GPIO.LOW)
+    #GPIO.output(Forward, GPIO.LOW)
+    #GPIO.output(Right, GPIO.LOW)
+    forward_motor.off()
+    right_motor.off()
 
 def left(x):
-    GPIO.output(Forward, GPIO.HIGH)
-    GPIO.output(Left, GPIO.HIGH)
+    #GPIO.output(Forward, GPIO.HIGH)
+    #GPIO.output(Left, GPIO.HIGH)
+    forward_motor.on()
+    left_motor.on()
     print("Moving Left")
     time.sleep(x)
-    GPIO.output(Forward, GPIO.LOW)
-    GPIO.output(Left, GPIO.LOW)
+    forward_motor.off()
+    left_motor.off()
+    #GPIO.output(Forward, GPIO.LOW)
+    #GPIO.output(Left, GPIO.LOW)
 
 start_time = time.clock()
 forward(5)
