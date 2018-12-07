153a8c2dbef0b12b07a2d0e21ab6414562c4a0f5
Debargha Ganguly <royal.debargha@gmail.com>
2018-12-07 09:17:23 +0530

    Finishing off AutoDrive

--- Build/AutoDrive/Drive.py
+++ Build/AutoDrive/Drive.py
@@ -5,6 +5,11 @@ import time
 import picamera
 import multiprocessing
 import datetime as dt
+from Predict import predict
+from Move import forward
+from Move import reverse
+from Move import right
+from Move import left
 
 camera = picamera.PiCamera()
 start_time=time.clock()
@@ -22,17 +27,21 @@ ultrasonic sensor based driving interrupts
 """
 
 def CaptureImage():
-    camera.capture("%s" %time.clock() + "jpg", resize=(150,150))
-    sleep(0.2)
+    image = camera.capture("%s" %time.clock() + "jpg", resize=(50,50))
+    image = cv2.resize(image, (50,50))
+    image = image.reshape(1, 50, 50, 3)
     print "Image captured on this process."
+    return image
 
-def Predict():
-    result = loaded_model.predict_classes(image)
-    if result='FF':
-        forward(5)
-    elif result="FL"
-        left(5)
-    elif result='FR'
-        right(5)
+def PredictAndMove(image):
+    result = predict(image)
+    if result=1:
+        forward(1)
+    elif result=2
+        left(1)
+    elif result=3
+        right(1)
 
-def Move()
+while (True):
+    image=CaptureImage()
+    PredictAndMove(image)
--- Build/AutoDrive/Move.py
+++ Build/AutoDrive/Move.py
@@ -3,7 +3,7 @@ Utility library to create move in any directionself.
 
 Call Move.direction(x) from the CarBrain to control the movement of the carself.
 """
-
+"""
 import sys
 import time
 import RPi.GPIO as GPIO
@@ -25,11 +25,13 @@ left_motor = OutputDevice(Left)
 sleeptime=1
 
 """
+"""
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(Forward, GPIO.OUT)
 GPIO.setup(Backward, GPIO.OUT)
 GPIO.setup(Right, GPIO.OUT)
 GPIO.setup(Left, GPIO.OUT)
+"""
 """
 
 def forward(x):
@@ -73,3 +75,65 @@ def left(x):
 
 
 GPIO.cleanup()
+"""
+
+
+"""The above code doesn't function due to dependeny issues with the GPIO library of the Raspberry Pi"""
+
+import sys
+import time
+import RPi.GPIO as GPIO
+from gpiozero import OutputDevice
+
+#mode=GPIO.getmode()
+
+Forward=13
+Backward=12
+Right = 15
+Left = 16
+
+forward_motor = OutputDevice(Forward)
+backward_motor = OutputDevice(Backward)
+right_motor = OutputDevice(Right)
+left_motor = OutputDevice(Left)
+
+
+def forward(x):
+    #GPIO.output(Forward, GPIO.HIGH)
+    forward_motor.on()
+    print("Moving Forward")
+    time.sleep(x)
+    forward_motor.off()
+    #GPIO.output(Forward, GPIO.LOW)
+
+def reverse(x):
+    #GPIO.output(Backward, GPIO.HIGH)
+    backward_motor.on()
+    print("Moving Backward")
+    time.sleep(x)
+    backward_motor.off()
+    #GPIO.output(Backward, GPIO.LOW)
+
+def right(x):
+    #GPIO.output(Forward, GPIO.HIGH)
+    #GPIO.output(Right, GPIO.HIGH)
+    forward_motor.on()
+    right_motor.on()
+    print("Moving Right")
+    time.sleep(x)
+    #GPIO.output(Forward, GPIO.LOW)
+    #GPIO.output(Right, GPIO.LOW)
+    forward_motor.off()
+    right_motor.off()
+
+def left(x):
+    #GPIO.output(Forward, GPIO.HIGH)
+    #GPIO.output(Left, GPIO.HIGH)
+    forward_motor.on()
+    left_motor.on()
+    print("Moving Left")
+    time.sleep(x)
+    forward_motor.off()
+    left_motor.off()
+    #GPIO.output(Forward, GPIO.LOW)
+    #GPIO.output(Left, GPIO.LOW)
